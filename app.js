2a79d60173cb13496edd425d23a715825ffca35c
1at24cs054-rgb <1at24cs054@atria.edu>
2026-01-09 12:27:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+async function fetchEmotion() {
+  try {
+    const response = await fetch(
+      "https://moodmate-backend.onrender.com/latest-emotion"
+    );
+
+    const data = await response.json();
+
+    document.getElementById("emotion").innerText =
+      "Emotion: " + data.emotion;
+
+    document.getElementById("confidence").innerText =
+      "Confidence: " + (data.confidence * 100).toFixed(1) + "%";
+
+  } catch (err) {
+    console.error("Failed to fetch emotion", err);
+  }
+}
+
+// fetch every 5 seconds
+setInterval(fetchEmotion, 5000);
+fetchEmotion();
